27b80d331728783130e7336ff0f82eca1b52e29a
HK-Rathnayaka <43449972+HK-Rathnayaka@users.noreply.github.com>
2019-09-18 23:35:55 +0530

    Size add method

--- src/spm_/Size.java
+++ src/spm_/Size.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
  *
  * @author hratlk
  */
-<<<<<<< HEAD
 
 
 import java.io.*;
@@ -21,22 +20,20 @@ import java.io.*;
  * @author User
  */
 //Main Function
-public class ArrayList_Testing {
+
 
     /**
      * @param args the command allLines arguments
      */
-=======
 public class Size {
-<<<<<<< HEAD
+
      public int analizeComplexityDueToSize(String line) {
        System.out.print("Size");
         return 0;
     }
-=======
+public class ArrayList_Testing {}
     //malshani
->>>>>>> 8682c4c0f1ca36dba31bf43d5889f1d16de95ca7
->>>>>>> d353e6e15c54851fec5f702cd803cad80a77878f
+
 }
 
 
